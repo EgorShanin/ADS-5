7ffdc353f580bfb4f0cb5028352d7fbe8a3b9db0
EgorShanin <94217235+EgorShanin@users.noreply.github.com>
2022-04-01 22:12:06 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,6 +4,36 @@
 
 template<typename T, int size>
 class TStack {
-
+private:
+    int first =-1;
+    int last = size;
+    int arr[size];
+public:
+    bool push(int x) {
+        if (first >= (last - 1)) {
+            return false;
+        } else {
+            arr[++first] = x;
+            return true;
+        }
+    }
+    int pop() {
+        if (first < 0) {
+            return 0;
+        }
+        else {
+            int x = arr[first--];
+            return x;
+        }
+    }
+    int top() {
+        return arr[first];
+    }
+    bool isEmpty() {
+        return (first < 0);
+    }
+    bool isFull() {
+        return (first == last - 1);
+    }
 };
 #endif  // INCLUDE_TSTACK_H_
